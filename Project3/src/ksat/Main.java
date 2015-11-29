7ce62c4fa3f0d76f894945cd5414cc773c7db67a
Kaspar-David Buss <kaspardavid.buss@informatik.uni-luebeck.de>
2015-11-30 00:10:40 +0100

    Implemented Solver; changed Solver to work with long
    Completed implementation of Random-3-SAT and Modified-Random-3-SAT.
    Solver now works with long values instead of boolean arrays as
    interpretations of the formula.

--- Project3/src/ksat/Main.java
+++ Project3/src/ksat/Main.java
@@ -1,5 +1,6 @@
 package ksat;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -7,10 +8,12 @@ public class Main {
 	/**
 	 * Reads in a 3-KNF formula from the standard input. The input is the number of variables, then a new line, followed by the
 	 * number of clauses, then a new line. After this, each 3-KNF clause is on a new line written as a space-delimited list while each
-	 * literal is a number. Positive numbers indicate the variables, negative ones indicate the negated variables. For example, the clause<br>
+	 * literal is a number. Positive numbers indicate non-negated variables, negative ones indicate the negated variables. For example,
+	 * the clause<br>
 	 * x2 v not(x4) v x6<br>
 	 * would be written as<br>
-	 * 2 -4 6
+	 * 2 -4 6<br>
+	 * Hence, variables are 1-indexed.
 	 */
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
@@ -20,19 +23,31 @@ public class Main {
 		for (int i = 0; i < noOfClauses; i++){
 			for (int j = 0; j < 3; j++){
 				int v = sc.nextInt();
-				if ((v > noOfVars) || (-v > noOfVars)){
+				if ((v > noOfVars) || (-v > noOfVars) || (v == 0)){
 					sc.close();
-					throw new IllegalArgumentException("Variable index too high.");
+					throw new IllegalArgumentException("Variable index too high or zero.");
 				}
 				clauses[i][j] = v;
 			}
 		}
 		sc.close();
-		new Solver(clauses,noOfVars);
+		Solver s = new Solver(clauses,noOfVars);
+		s.solve();
 	}
 	
-	public static int getRunsOfModKSat(int n){
-		double p = 0.5 * Math.pow(0.75, n);
-		return (int) Math.ceil(30.0 / p);
+	private static int[][] generateRandomClauses(int noOfVars, int noOfClauses){
+		int[][] clauses = new int[noOfClauses][3];
+		Random rand = new Random();
+		for (int i = 0; i < noOfClauses; i++){
+			for (int j = 0; j < 3; j++){
+				int var = rand.nextInt(noOfVars)+1;
+				if (rand.nextBoolean()){
+					clauses[i][j] = var;
+				} else {
+					clauses[i][j] = -var;
+				}
+			}
+		}
+		return clauses;
 	}
 }
--- Project3/src/ksat/Solver.java
+++ Project3/src/ksat/Solver.java
@@ -1,29 +1,140 @@
 package ksat;
 
+import java.util.Random;
+
 public class Solver {
 	private int[][] clauses;
-	private boolean[] interp;
+	private int noOfVars;
+	private Random rand;
+	
+	/**
+	 * Instantiates a new Solver for a 3-KNF formula.
+	 * @param clauses The clauses in the formula. Each array in <tt>clauses</tt> corresponds to one clause. A positive value in the clause
+	 * corresponds to a non-negated literal, a negative value to a negated literal of the variable that is the value's absolute.
+	 * @param noOfVars The number of variables in the formula.
+	 */
 	public Solver(int[][] clauses, int noOfVars){
+		rand = new Random();
 		this.clauses = clauses;
-		System.out.println(satisfies());
-		interp = new boolean[noOfVars];
+		this.noOfVars = noOfVars;
 	}
 	
-	public boolean satisfies(){
+	/**
+	 * Attempts to solve the given formula with the Modified Random-3-SAT algorithm with an error probability of at most 10^(-13).
+	 */
+	public void solve(){
+		int restarts = getNoOfRestarts();
+		Long interp = modifiedRandomKSat(restarts);
+		if (interp == null){
+			System.out.println("No satisfying interpretation could be found.");
+		} else {
+			System.out.println("A satisfying interpretation is "+interpretationToString(interp));
+		}
+	}
+	
+	/**
+	 * Executes the Modified Random-3-SAT algorithm.
+	 * @param r The number of restarts in Modified Random-3-SAT. 
+	 * @return A satisfying interpretation of the formula if such a one is found; <tt>null</tt> otherwise.
+	 */
+	private Long modifiedRandomKSat(int r){
+		int l = 3 * noOfVars;
+		for (int i = 0; i < r; i++){
+			Long interp = randomKSat(l);
+			if (interp != null){
+				return interp;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Executes the Random-3-SAT algorithm.
+	 * @param l The number of iterations of Random 3-SAT.
+	 * @return A satisfying interpretation of the formula if such a one is found; <tt>null</tt> otherwise.
+	 */
+	private Long randomKSat(int l){
+		long interp = getRandomInterpretation();
+		for (int i = 0; i < l; i++){
+			int[] failedClause = satisfies(interp);
+			if (failedClause == null){
+				return interp;
+			}
+			flipRandomVar(failedClause, interp);
+		}
+		return null;
+	}
+	
+	/**
+	 * Generates a random interpretation.
+	 * @return A random interpretation.
+	 */
+	private long getRandomInterpretation(){
+		return rand.nextLong();
+	}
+	
+	/**
+	 * Tests if an interpretation satisfies the clauses.
+	 * @param interp An interpretation.
+	 * @return A clause that is not satisfied by the interpretation if this interpretation does not satisfy all clauses,
+	 * <tt>null</tt> otherwise.
+	 */
+	private int[] satisfies(long interp){
 		for (int[] clause : clauses){
 			boolean sat = false;
 			for (int i = 0; i < 3; i++){
 				int lit = clause[i];
 				int var = Math.abs(lit);
-				if (((interp[var-1]) && (lit > 0)) || ((!interp[var-1]) && (lit < 0))){
+				boolean varTrue = (((1L << (var-1)) & interp) != 0);
+				if ((varTrue && (lit > 0)) || (!varTrue && (lit < 0))){
 					sat = true;
 					break;
 				}
 			}
 			if (!sat){
-				return false;
+				return clause;
 			}
 		}
-		return true;
+		return null;
+	}
+	
+	/**
+	 * From a given clause, chooses a random literal and flips the corresponding variable in the interpretation.
+	 * @param clause The given clause.
+	 * @param interp An interpretation of the formula.
+	 * @return The interpretation with a bit flipped that corresponds to a literal in <tt>clause</tt>.
+	 */
+	private long flipRandomVar(int[] clause, long interp){
+		int var = Math.abs(clause[rand.nextInt(3)]);
+		return interp ^ (1L << (var-1));
+	}
+	
+	/**
+	 * Calculates the necessary number of restarts in Modified k-SAT for this formula.
+	 * @return The necessary number of restarts in Modified k-SAT
+	 */
+	private int getNoOfRestarts(){
+		double p = 0.5 * Math.pow(0.75, noOfVars);
+		return (int) Math.ceil(30.0 / p);
+	}
+	
+	/**
+	 * Generates a String representation of an interpretation.
+	 * @param interp The <tt>long</tt> representation of an interpretation of the formula.
+	 * @return A string starting with the character '[' and ending with ']'. In between those is a comma-delimited list of boolean values, 
+	 * one for each variable. If the ith-lowest bit of the interpretation is a 1, then the ith list entry is "true", otherwise "false".
+	 */
+	private String interpretationToString(long interp){
+		String result = "[";
+		for (int i = 0; i < noOfVars; i++){
+			boolean varTrue = (((1L << i) & interp) != 0);
+			if (varTrue){
+				result += "true, ";
+			} else {
+				result += "false, ";
+			}
+		}
+		result = result.substring(0,result.length()-2)+"]";
+		return result;
 	}
 }
